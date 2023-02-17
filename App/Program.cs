83d95c8bd6860e61e0615ab4a3c207ac78702124
Saeed Rahmani <saeedrahmo@gmail.com>
2023-02-17 11:52:11 -0500

    updated

---
+++ App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace App // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var counter = 0;
+            var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max is -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+            }
+        }
+    }
+}
